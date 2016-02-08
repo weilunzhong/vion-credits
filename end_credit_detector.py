16396044c5fc9e8d93496c0dfd0f08ed85c02a68
weilunzhong <weilun@kth.se>
2016-02-08 16:58:39 +0100

    finished with end credit, params needs to tune

--- end_credit_detector.py
+++ end_credit_detector.py
@@ -1,5 +1,5 @@
 from vionrnd.models import FrameHistogram
-
+import numpy as np
 
 
 
@@ -13,17 +13,40 @@ class EndCreditDetector(object):
         dominating_ratio = [self.single_frame_hist_dominance(i) for i in frame_hists]
         return np.asarray(dominating_ratio)
 
-    def array_sliding_window(self, result_array, window_size, filter_ratio):
-        array_length = np.size(result_array)
-        buffer_array = np.zeros((array_length-window_size), dtype = np.int)
-        flag_array = np.zeros((array_length-window_size), dtype = np.int)
+    def array_sliding_window(self, frame_hists, time_stamps, window_size=5):
+        array_length = np.size(frame_hists)
+        smoothed_array = np.zeros((array_length-window_size), dtype = np.float)
         for idx in range(0, array_length - window_size):
-            window_score = np.sum(result_array[idx:idx+window_size])
-            buffer_array[idx] = (1 if window_score>(window_size*filter_ratio) else 0)
-            if buffer_array[idx] < buffer_array[idx]
-        buffer_array = np.delete(buffer_array, 0)
-        flag_array = np.zeros(np.size(buffer_array), dtype=np.int)
-        return flag_array  
+            smoothed_array[idx] = float(np.sum(frame_hists[idx:idx+window_size]))/window_size
+        return smoothed_array, time_stamps[0:array_length-window_size]
             
-def getWords(text):
-    return re.compile("\w+").findall(text)
+    """ dynamic thresholding that doesnt work well for now
+    def interpolation(self, smoothed_array, time_stamps, threshold):
+        max_ratio, min_ratio  =  np.max(smoothed_array), np.min(smoothed_array)
+        print max_ratio, min_ratio
+        interpolation_value = min_ratio + threshold * (max_ratio - min_ratio)
+        print interpolation_value
+        estimation_func = np.poly1d(np.polyfit(range(0, len(smoothed_array)), smoothed_array, 7))
+        self.estimation_func =estimation_func
+        print estimation_func(range(0,len(smoothed_array)))
+        idx_array = np.where(estimation_func(range(0,len(smoothed_array))) > interpolation_value)
+        print "index {0} and the timestamp {1}.".format(idx_array[0][0], time_stamps[idx_array[0][0]])
+        return time_stamps[idx_array[0]]
+        """
+
+    def interpolation(self, smoothed_array, time_stamps, threshold):
+        max_ratio, min_ratio  =  np.max(smoothed_array), np.min(smoothed_array)
+        if max_ratio - min_ratio < 0.5:
+            raise VionCreditError("The hist ratio max {0} and min {1} is too close, check for wind back time.".format(max_ratio, min_ratio) )
+        interpolation_value = min_ratio + threshold * (max_ratio - min_ratio)
+        filtered_array = [(1 if ele > interpolation_value else 0) for ele in smoothed_array]
+        pre_value = 0
+        start_index, end_index = [], []
+        for idx, ratio in enumerate( filtered_array):
+            if ratio > pre_value:
+                start_index.append(idx)
+            if ratio < pre_value:
+                end_index.append(idx)
+            pre_value = ratio
+        print time_stamps[start_index]
+        print time_stamps[end_index]
--- frame_hist_calculator.py
+++ frame_hist_calculator.py
@@ -3,6 +3,7 @@ import pysrt
 #from vionrnd.models import FrameHistogram
 from vionaux.rnd import vidioids
 import re
+from end_credit_detector import EndCreditDetector
 
 class FrameHistCalculator(object):
 
@@ -17,22 +18,23 @@ class FrameHistCalculator(object):
 
     def frame_reader(self, path, sampling_rate=1, wind_back_time=-300):
         frame_generator = self.VVH.get_frames(path, sampling_rate, wind_back_time)
-        frame_hist = None
+        frame_hist = []
         time_stamps = []
-        for (frame, time_stamp) in frame_generator:
-            hist = self.calculate_histogram(frame)
-            hist = np.expand_dims(hist,axis=0)
-            if frame_hist == None:
-                frame_hist = hist
-            else:
-                frame_hist = np.append(frame_hist, hist, axis=0)
+        for frame, time_stamp in frame_generator:
+            frame_hist.append(self.calculate_histogram(frame))
             time_stamps.append(time_stamp)
+        frame_hist = np.asarray(frame_hist)
         time_stamps = np.asarray(time_stamps)
-#        print frame_hist.shape, len(time_stamps)
-        return FrameHistogram(data=frame_hists , time_stamps=time_stamps)
+        return frame_hist, time_stamps
+#        return FrameHistogram(data=frame_hists , time_stamps=time_stamps)
 
 
 if __name__ == "__main__":
     FHC = FrameHistCalculator()
+    ECD = EndCreditDetector()
     video_path = "/mnt/movies03/boxer_movies/tt3247714/Survivor (2015)/Survivor.2015.720p.BluRay.x264.YIFY.mp4"
-    hist_generator = FHC.frame_reader(video_path, 0.1)
+    frame_hist, time_stamps = FHC.frame_reader(video_path, 0.1, -900)
+    dominant_array = ECD.frame_hist_dominance(frame_hist)
+    sliding_window, time_stamps = ECD.array_sliding_window(dominant_array, time_stamps, 3)
+    print sliding_window
+    ECD.interpolation(sliding_window, time_stamps, 0.7)
