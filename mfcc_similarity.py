991016319f39bf669ad3a7542e3b497bd02f43f7
weilunzhong <weilun@kth.se>
2016-02-10 16:23:30 +0100

    added semi auto start hunter and modified the automatic version.

--- mfcc_similarity.py
+++ mfcc_similarity.py
@@ -5,41 +5,58 @@ from vionaux.rnd import audioids
 class StartCreditDetector(object):
 
     def _window_similarity(self, mfcc_1, mfcc_2, window_size):
-        assert (mfcc_1.shape == mfcc_2,shape), "both mfccs must have the same dimension"
         similarity_score = [distance.euclidean(mfcc_1[i][1:], mfcc_2[i][1:]) for i in range(0, window_size)]
         return similarity_score
 
     def position_loop(self, base_mfcc, income_mfcc):
         assert (base_mfcc.shape == income_mfcc.shape), "both mfccs must have the same dimension"
         current_score = 1e6
+        sample_dist = 20
+        window_size = 20
         for idx1 in range(0,(len(base_mfcc)/sample_dist)):
             mfcc_1 = base_mfcc[idx1*sample_dist:idx1*sample_dist+window_size]
             sim_score_list = []
             for idx2 in range(0, len(base_mfcc)-2*window_size):
-                mfcc_2 = income_mfcc[idx2*sample_dist:idx2*sample_dist+window_size]
+                mfcc_2 = income_mfcc[idx2:idx2+window_size]
                 sim_score = sum(self._window_similarity(mfcc_1, mfcc_2, window_size))
                 sim_score_list.append(sim_score)
 
             if min(sim_score_list) < current_score and min(sim_score_list) > 10:
                 current_score = min(sim_score_list)
-                matched_base_idx = np.argmin(sim_score_list)
-                
+                matched_base_idx = idx1*sample_dist
+                matched_income_idx = np.argmin(sim_score_list)
+        print current_score, matched_base_idx, matched_income_idx 
+        self.shift_diff = matched_base_idx - matched_income_idx
+        print self.shift_diff
 
-    def lenth_extraction(self):
-        if shift_diff < 0:
-            truncated_base_mfcc = base_mfcc[shift_diff:]
+    def lenth_extraction(self,base_mfcc, income_mfcc, base_timestamp, income_timestamp):
+        if self.shift_diff > 0:
+            truncated_base_mfcc = base_mfcc[self.shift_diff:]
+            truncated_base_timestamp = base_timestamp[self.shift_diff:]
             truncated_income_mfcc = income_mfcc[0:len(truncated_base_mfcc)]
+            truncated_income_timestamp = income_timestamp[0:len(truncated_base_mfcc)]
         else:
-            truncated_income_mfcc = income_mfcc[abs(shift_diff):]
+            truncated_income_mfcc = income_mfcc[abs(self.shift_diff):]
+            truncated_income_timestamp = income_timestamp[abs(self.shift_diff):]
             truncated_base_mfcc = base_mfcc[0:len(truncated_income_mfcc)]
+            truncated_base_timestamp = base_timestamp[0:len(truncated_income_mfcc)]
 
-        sim_score_list = _window_similarity(truncated_base_mfcc, truncated_income_mfcc, len(truncated_base_mfcc))
+        sim_score_list = self._window_similarity(truncated_base_mfcc, truncated_income_mfcc, len(truncated_base_mfcc))
+        print [int(i) for i in sim_score_list]
+        start_credit_index = [i for i, value in enumerate(sim_score_list) if value < 30]
+        print "s3e1: ", truncated_base_timestamp[start_credit_index]
+        print "s3e2: ", truncated_income_timestamp[start_credit_index]
+        print "diff in the two: ", self.shift_diff
+
         
 
 if __name__ == "__main__":
     path_1 = "/media/weilun/tv_series/audio_file/house_of_cards_1.wav"
-    path_2 = "/media/weilun/tv_series/audio_file/house_of_cards_2.wav"
+    path_2 = "/media/weilun/tv_series/audio_file/house_of_cards_4.wav"
     VAH = audioids.VionAudioHandler()
-    base_mfcc, time_stamp1 = VAH.get_mfccs(path_1, 0, 60, 14000, 2048, 1400)
-    income_mfcc, time_stamp2 = VAH.get_mfccs(path_2, 0, 60, 14000, 2048, 1400)
-    print base_mfcc.shape
+    SCD = StartCreditDetector()
+    base_mfcc, time_stamp1 = VAH.get_mfccs(path_1, 0, 300, 14000, 2048, 1400)
+    income_mfcc, time_stamp2 = VAH.get_mfccs(path_2, 0, 300, 14000, 2048, 1400)
+    print "here is the mfcc length: {0}".format(len(base_mfcc))
+    SCD.position_loop(base_mfcc, income_mfcc)
+    SCD.lenth_extraction(base_mfcc, income_mfcc, time_stamp1, time_stamp2)
---
+++ pursuit_of_credit.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.spatial import distance
+from vionaux.rnd import audioids
+
+class StartCreditHunter(object):
+
+    def _window_similarity(self, mfcc_1, mfcc_2, window_size):
+        similarity_score = [distance.euclidean(mfcc_1[i][1:], mfcc_2[i][1:]) for i in range(0, window_size)]
+        return similarity_score
+
+    def locate_credit_position(self, base_mfcc, income_mfcc, base_timestamp, income_timestamp):
+        credit_length = len(base_timestamp)
+        sim_score_list = []
+        for idx in range(0, len(income_mfcc)-credit_length):
+            sim_score = self._window_similarity(base_mfcc, income_mfcc[idx:idx+credit_length], credit_length)
+            sim_score_list.append(sum(sim_score))
+        sims = np.asarray(sim_score_list)
+        start_index = np.argmin(sims)
+        print sims, income_timestamp[start_index]
+        print start_index
+
+if __name__ == "__main__":
+    path_1 = "/media/weilun/tv_series/audio_file/humans_1.wav"
+    path_2 = "/media/weilun/tv_series/audio_file/humans_2.wav"
+    VAH = audioids.VionAudioHandler()
+    SCH = StartCreditHunter()
+    base_mfcc, time_stamp1 = VAH.get_mfccs(path_1, 71, 115, 14000, 2048, 1400)
+    income_mfcc, time_stamp2 = VAH.get_mfccs(path_2, 0, 300, 14000, 2048, 1400)
+    print "here is the mfcc length: {0}".format(len(base_mfcc))
+    SCH.locate_credit_position(base_mfcc, income_mfcc, time_stamp1, time_stamp2)
